32fba9c1eca59cd63320e544db2822efcb18d592
louisk2807 <92081558+louisk2807@users.noreply.github.com>
2023-05-20 11:30:01 +0700

    Delete src/main/java/day8 directory

--- src/main/java/day8/B1/Drawable.java
+++
@@ -1,9 +0,0 @@
-package day8.B1;
-
-public class Drawable {
-    public void draw(IHinhHoc hinhHoc) {
-        System.out.println("Vẽ ra " + hinhHoc.getClass().getSimpleName() + ":");
-        System.out.println("Diện tích: " + hinhHoc.tinhDienTich());
-        System.out.println("Chu vi: " + hinhHoc.tinhChuVi());
-    }
-}
--- src/main/java/day8/B1/IHinhHoc.java
+++
@@ -1,6 +0,0 @@
-package day8.B1;
-
-public interface IHinhHoc {
-    double tinhChuVi();
-    double tinhDienTich();
-}
